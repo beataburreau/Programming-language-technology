23adc9494d7bc847a1fda32a85cdbb701b30ee8e
Beata Burreau <burreauburreau@gmail.com>
2020-11-30 12:30:18 +0100

    fixed good test cases

--- lab2/Interpreter.hs
+++ lab2/Interpreter.hs
@@ -12,8 +12,12 @@ import CMM.ErrM
 data Val = VBool BoolLit
     | VInt Integer
     | VDouble Double
-    deriving (Prelude.Eq, Prelude.Ord, Prelude.Show, Prelude.Read)
+    deriving (Prelude.Eq, Prelude.Ord, Prelude.Read)
 
+instance Show Val where
+    show (VBool a) = show a
+    show (VInt a) = show a
+    show (VDouble a) = show a
 
 type Env = ([Def], [[(Id, Val)]])
 
@@ -28,18 +32,29 @@ predefinedFunctions :: [Def]
 predefinedFunctions = [DFun Type_void (Id "printInt") [ADecl Type_int (Id "x")] [], DFun Type_void (Id "printDouble") [ADecl Type_double (Id "x")] [], 
                         DFun Type_int (Id "readInt") [] [], DFun Type_double (Id "readDouble") [] []]
 
-evalArgs :: [Arg] -> [Exp] -> [(Id, Val)]
-evalArgs = undefined
+evalArgs :: ([(Id, Val)], Env) -> [Arg] -> [Exp] -> IO ([(Id, Val)], Env)
+evalArgs (vals, env) [] [] = return (vals, env)
+evalArgs (vals, env) ((ADecl typ id):as) (e:es) = do 
+    (v, env2) <- eval e env 
+    if typ == Type_double
+        then 
+            evalArgs ((id, toDouble v):vals, env2) as es
+        else    
+            evalArgs ((id, v):vals, env2) as es
 
 eval  :: Exp   -> Env -> IO (Val, Env)
 eval (EBool b) env = return (VBool b, env)
 eval (EInt i) env = return (VInt i, env)
 eval (EDouble g) env = return (VDouble g, env)
 eval (EId id) env = return (lookupVal id env, env)
-eval (EApp (Id id) [n]) env | id == "printInt" || id == "printDouble" = do
+eval (EApp (Id id) [n]) env | id == "printInt" = do
     (str, env1) <- eval n env
     print str
     return (VBool LTrue, env1)
+eval (EApp (Id id) [n]) env | id == "printDouble" = do
+    (str, env1) <- eval n env
+    print (toDouble str)
+    return (VBool LTrue, env1)    
 eval (EApp (Id "readInt") []) env = do
     input <- getLine
     return (VInt (read input), env)
@@ -47,11 +62,12 @@ eval (EApp (Id "readDouble") []) env = do
     input <- getLine
     return (VDouble (read input), env)
 eval (EApp id exps) (defs, vals) = do
-    r <- execs stms (defs, evalArgs args exps :vals)
+    (newVals, env) <- evalArgs ([], (defs, vals)) args exps
+    r <- execs stms (defs, newVals:vals)
     case r of
-        Left val -> return (val, (defs, vals))
-        Right env -> return (VBool LTrue, env)
-    where DFun _ _ args stms = lookupFun id (defs, vals)
+        Left val -> if typ == Type_double then return (toDouble val, env) else return (val, env)
+        Right _ -> return (VBool LTrue, env)
+    where DFun typ _ args stms = lookupFun id (defs, vals)
 eval (EPost id op) (defs, vals) = return (val, (defs, updateVal vals id (incDecVal val op)))
     where val = lookupVal id (defs, vals)
 eval (EPre op id) (defs, vals) = return (val, (defs, updateVal vals id val))
@@ -71,33 +87,59 @@ eval (EAdd exp1 op exp2) env = do
 eval (ECmp exp1 op exp2) env = do
     (v1, e1) <- eval exp1 env
     (v2, e2) <- eval exp2 e1
-    case op of
-        OLt -> return (toBoolLit (v1 < v2), e2)
-        OGt -> return (toBoolLit (v1 > v2), e2)
-        OLtEq -> return (toBoolLit (v1 <= v2), e2)
-        OGtEq -> return (toBoolLit (v1 >= v2), e2)
-        OEq -> return (toBoolLit (v1 == v2), e2)
-        ONEq -> return (toBoolLit (v1 /= v2), e2)
+    return (toBoolLit (compareVal v1 op v2), e2)
 eval (EAnd exp1 exp2) env = do
     (v1, e1) <- eval exp1 env
-    (v2, e2) <- eval exp2 e1
-    return (vAnd v1 v2, e2)
+    if v1 == VBool LFalse
+        then
+            return (VBool LFalse, e1)
+        else do
+            (v2, e2) <- eval exp2 e1
+            return (vAnd v1 v2, e2)
 eval (EOr exp1 exp2) env = do
     (v1, e1) <- eval exp1 env
-    (v2, e2) <- eval exp2 e1
-    return (vOr v1 v2, e2)
+    if v1 == VBool LTrue
+        then 
+            return (VBool LTrue, e1)
+        else do
+            (v2, e2) <- eval exp2 e1
+            return (vOr v1 v2, e2)
 eval (EAss id exp) env = do
     (v1, (defs, vals)) <- eval exp env 
-    return (v1, (defs, updateVal vals id v1))
+    if lookupType id env == Type_double
+        then
+            return (toDouble v1, (defs, updateVal vals id (toDouble v1)))
+        else
+            return (v1, (defs, updateVal vals id v1))
+
+compareVal :: Val -> CmpOp -> Val -> Bool
+compareVal (VBool v1) op (VBool v2) = compOp op (VBool v1) (VBool v2)
+compareVal v1 op v2 = compOp op (toDouble v1) (toDouble v2)
+
+toDouble :: Val -> Val
+toDouble (VInt a) = VDouble (fromIntegral a)
+toDouble (VDouble a) = VDouble a
+
+compOp :: CmpOp -> Val -> Val -> Bool
+compOp OLt a b = a < b
+compOp OGt a b = a > b
+compOp OLtEq a b = a <= b
+compOp OGtEq a b = a >= b
+compOp OEq a b = a == b
+compOp ONEq a b = a /= b
 
 updateVal :: [[(Id, Val)]] -> Id -> Val -> [[(Id, Val)]]
-updateVal = updteVal [[]]
+updateVal env1 id val = env2
+    where (_:env2) = updteVal [[]] env1 id val
 
 updteVal :: [[(Id, Val)]] -> [[(Id, Val)]] -> Id -> Val -> [[(Id, Val)]]
 updteVal _ [] _ _ = undefined
 updteVal head (t:tail) id val | elem id (map fst t) = head ++ [map (\(i,v) -> if i == id then (i,val) else (i,v)) t] ++ tail 
 updteVal head (t:tail) id val = updteVal (head ++ [t]) tail id val
 
+-- [] [4] [3] [2] [1]
+-- [4] 3 [2] [1]
+-- [3, 4]
 toBoolLit :: Bool -> Val
 toBoolLit True = VBool LTrue
 toBoolLit False = VBool LFalse
@@ -115,10 +157,18 @@ times (VInt a) (VInt b) = VInt (a * b)
 divide :: Val -> Val -> Val
 divide (VDouble a) (VDouble b) = VDouble (a / b)
 divide (VInt a ) (VInt b) = VInt (div a b)
+divide (VDouble a) (VInt b) = VDouble (a / d)
+    where (VDouble d) = toDouble (VInt b)
+divide (VInt a) (VDouble b) = VDouble (d / b)
+    where (VDouble d) = toDouble (VInt a)
 
 plus :: Val -> Val -> Val
 plus (VDouble a) (VDouble b) = VDouble (a + b)
 plus (VInt a ) (VInt b) = VInt (a + b)
+plus (VDouble a) (VInt b) = VDouble (a + d)
+    where (VDouble d) = toDouble (VInt b)
+plus (VInt a) (VDouble b) = VDouble (d + b)
+    where (VDouble d) = toDouble (VInt a)
 
 minus :: Val -> Val -> Val
 minus (VDouble a) (VDouble b) = VDouble (a - b)
@@ -139,9 +189,13 @@ exec (SExp exp) env = do
 exec (SDecls typ ids) (defs, block:vals) = do
     return (Right (defs, (vars ++ block):vals))
     where vars = zip ids (repeat (initVal typ))
-exec (SInit _ id exp) (defs, vals) = do
+exec (SInit typ id exp) (defs, vals) = do
     (v, (defs1, block:vals1)) <- eval exp (defs, vals)
-    return (Right (defs1, ((id, v):block):vals1))
+    if typ == Type_double
+        then 
+            return (Right (defs1, ((id, toDouble v):block):vals1))
+        else
+            return (Right (defs1, ((id, v):block):vals1))
 exec (SReturn exp) env = do
     (v, _) <- eval exp env
     return (Left v)
@@ -153,7 +207,7 @@ exec (SBlock stms) (defs, vals) = do
     r <- execs stms (defs, []:vals)
     case r of
         Left v -> return (Left v)
-        Right (defs, _:vals) -> return (Right (defs, vals))
+        Right (defs, _:newVals) -> return (Right (defs, newVals))
 exec (SIfElse exp stm1 stm2) env = do
     (v, env1) <- eval exp env
     if isTrue v
@@ -183,6 +237,13 @@ execs (s:ss) (defs, vals) = do
     case r of
         Left  v    -> return (Left v)
         Right (_,vals1) -> execs ss (defs, vals1)
+
+lookupType :: Id -> Env -> Type
+lookupType id (defs, block:vals) = case lookup id block of
+    Just (VDouble _) -> Type_double
+    Just (VInt _) -> Type_int
+    Just (VBool _) -> Type_bool
+    Nothing -> lookupType id (defs, vals) 
 
 lookupVal :: Id -> Env -> Val
 lookupVal id (defs, block:vals) = case lookup id block of
