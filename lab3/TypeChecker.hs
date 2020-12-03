d83768bf051b02454f8b1db28a1c8040ca93e3d2
Beata Burreau <burreauburreau@gmail.com>
2020-12-03 15:55:54 +0100

    Initial commit lab 3

---
+++ lab3/TypeChecker.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase       #-}
+{-# LANGUAGE PatternSynonyms  #-}
+{-# LANGUAGE TupleSections    #-}
+
+-- | Type checker for C--, producing typed syntax from ASTs.
+
+module TypeChecker where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Except
+import Control.Monad.Reader
+import Control.Monad.State
+
+import Data.Functor
+import Data.Maybe
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+import CMM.Abs
+import CMM.Print (printTree)
+
+import qualified Annotated as A
+
+type TypeError = String
+
+-- | Entry point of type checker.
+
+typecheck :: Program -> Either TypeError A.Program
+typecheck prg = return prg
